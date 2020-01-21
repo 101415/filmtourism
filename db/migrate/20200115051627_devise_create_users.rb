eaf666c1698dfedd7950e7b0284c477ccac57ed9
101415 <j.chicharito.h.0014@gmail.com>
2020-01-21 16:55:29 +0900

    commit

--- db/migrate/20200115051627_devise_create_users.rb
+++ db/migrate/20200115051627_devise_create_users.rb
@@ -20,7 +20,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :last_sign_in_at
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
-
+      t.string :users, :provider
+      t.string :users, :uid
+      t.string :users, :token
+      t.string :users, :meta
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
