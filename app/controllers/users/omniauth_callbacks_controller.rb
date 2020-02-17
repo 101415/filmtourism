f179bcf8429f4d77e8e0ad90ee43519c74b35527
101415 <j.chicharito.h.0014@gmail.com>
2020-02-17 11:50:50 +0900

    commit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,26 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  # end  
-
-  # def google
-  #   @user = User.from_omniauth(request.env['omniauth.auth'])
-
-  #   if @user.persisted?
-  #     # flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-  #     # redirect_to action: :index unless user_signed_in?
-  #     sign_in_and_redirect @user
-  #     set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-  #   else
-  #     session['devise.google_data'] = request.env['omniauth.auth']
-  #     redirect_to tweets_path 
-  #   end
-  # end
-
-
   def google
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
