14acef97876a2a26c61e84e81b0d2db5adfc1d0e
101415 <j.chicharito.h.0014@gmail.com>
2020-03-10 20:01:43 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets.page(params[:page]).per(4).order("created_at DESC")
+    @all_ranks = Tweet.find(Like.group(:tweet_id).order('count(tweet_id) desc').page(params[:page]).per(9).pluck(:tweet_id))
   end
 end
