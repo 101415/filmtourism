b0b9ffc9b331f183b1f287ffeae0df6efde62f77
101415 <j.chicharito.h.0014@gmail.com>
2020-03-10 19:58:50 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @all_ranks = Tweet.find(Like.group(:tweet_id).order('count(tweet_id) desc').page(params[:page]).per(4).pluck(:tweet_id))
+    @all_ranks = Tweet.find(Like.group(:tweet_id).order('count(tweet_id) desc').page(params[:page]).per(9).pluck(:tweet_id))
   end
   
   def show
